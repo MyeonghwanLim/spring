d8b89acbd69329e01eba16d81231333f04f7d2b7
user-PC\Home <dlaaod012@naver.com>
2022-09-13 21:51:11 +0900

    spring

---
+++ 0913/src/main/java/test/IPhone.java
@@ -0,0 +1,37 @@
+package test;
+
+public class IPhone implements Phone{
+	private AppleWatch watch;
+	private int number;
+	public IPhone() {
+		System.out.println("아이폰 객체생성완료");
+	}
+	public IPhone(AppleWatch watch) {
+		System.out.println("아이폰 객체생성완료22");
+		this.watch= watch;
+	}
+	public IPhone(AppleWatch watch,int number) {
+		System.out.println("아이폰 객체생성완료333");
+		this.watch= watch;
+		this.number=number;
+	}
+	@Override
+	public void powerOn() {
+		System.out.println("아이폰 전원 ON :"+this.number);
+	}
+	@Override
+	public void powerOff() {
+		System.out.println("아이폰 전원 OFF");
+	}
+	@Override
+	public void volumeUp() {
+		
+		watch.volumeUp();
+	}
+	@Override
+	public void volumeDown() {
+		
+		watch.volumeDown();
+	}
+	
+}
